2f0ab2d7bafac2dbd249cc0dc3517be4a587cf00
son1x <162944716+Son1x-7732@users.noreply.github.com>
2025-12-18 21:58:34 +0530

    Update 1.py

--- DSA/arrays/arr_med/1.py
+++ DSA/arrays/arr_med/1.py
@@ -1,1 +1,15 @@
 # Two Sum problem.
+arr = [1, 2, 4, 8, 6, 10]
+
+
+def two_sum(arr, n, target):
+    result = []
+    for i in range(n):
+        for j in range(i + 1, n):
+            if arr[i] + arr[j] == target:
+                result.append([i, j])
+                
+    return result
+
+
+print(two_sum(arr, len(arr), 14))
